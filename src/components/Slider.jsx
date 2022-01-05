197fce552276d90cf2579086bed477ea8c592899
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-01-05 15:01:13 -0300

    Agrega sliders

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,6 +7,8 @@ const Container = styled.div`
   widht: 100%;
   height: 100vh;
   display: flex;
+  background-color: coral;
+  position: relative;
 `;
 
 const Arrow = styled.div`
@@ -14,15 +16,24 @@ const Arrow = styled.div`
   height: 50px;
   background-color: #fff7f7;
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
 `;
 
 const Slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowLeftOutlinedIcon />
       </Arrow>
-      <Arrow>
+      <Arrow direction="right">
         <ArrowRightOutlinedIcon />
       </Arrow>
     </Container>
