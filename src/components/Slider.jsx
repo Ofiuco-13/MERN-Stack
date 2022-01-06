88a563f80562ebe98165735cb420c36f08c39bab
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-01-06 20:03:11 -0300

    Agrega container en el eje X

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
 import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+import Doctora from "../images/doctora.png";
 
 const Container = styled.div`
   widht: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -25,15 +26,97 @@ const Arrow = styled.div`
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(0vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+
+  const handleClick = (direction) => {
+
+  }
+
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlinedIcon />
       </Arrow>
-      <Arrow direction="right">
+      <Wrapper>
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src={Doctora} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>LOREM IPSUM</Title>
+            <Desc>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro,
+              nulla exercitationem!
+            </Desc>
+            <Button>SHOW MORE</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fcf1ed">
+          <ImgContainer>
+            <Image src={Doctora} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>LOREM IPSUM</Title>
+            <Desc>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro,
+              nulla exercitationem!
+            </Desc>
+            <Button>SHOW MORE</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlinedIcon />
       </Arrow>
     </Container>
