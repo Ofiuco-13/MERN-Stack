f6392192dbba8f457912c4dfbe4c65ec5b7eea69
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-01-10 18:58:01 -0300

    Agrega componente newsletter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,17 @@ import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
 
-export const Home = () => {
+const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
       <Slider />
       <Categories />
-      <Products/>
+      <Products />
+      <Newsletter />
     </div>
   );
 };
