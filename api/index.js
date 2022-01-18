0f614a1813b729a9cbc56a2014170db85fa7d5b4
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-01-18 18:52:41 -0300

    Conecta a la base de datos

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose"; //Cluster: EmmanuelMartinez - alanturing01
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, () => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Conexion exitosa a la DB"))
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Servidor funcionando en puerto 5000");
 });
