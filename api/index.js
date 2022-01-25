e96401a4241ca9f4959a3554b2948ede4e13fb96
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-01-25 16:25:49 -0300

    Agrega autenticación con AES.
    Para que no se puedan ver las passwords de los usuarios

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose"; //Cluster: EmmanuelMartinez - alanturing01
 import dotenv from "dotenv";
 import userRoute from "./routes/user.js";
+import authRoute from "./routes/auth.js";
 
 const app = express();
 dotenv.config();
@@ -16,6 +17,7 @@ mongoose
 app.use(express.json());
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Servidor funcionando en puerto 5000");
---
+++ api/routes/auth.js
@@ -0,0 +1,47 @@
+import express from "express";
+import User from "../models/User.js";
+import CryptoJS from "crypto-js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong Credentials");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong Credentials");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+export default router;
